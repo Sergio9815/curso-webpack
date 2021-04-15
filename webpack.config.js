a3bcb31ae6cdef151b8028103473b861d1032045
sergio9815 <gsergio1598@gmail.com>
2021-04-14 20:45:32 -0500

    :bug: Corrección de errores

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   // ubicacion del archivo donde se almacena la compilacion de webpack
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js', // IDENTIFICAR CADA COMPILACION DEL PROYECTO CON UN HASH
+    filename: 'index.js', // IDENTIFICAR CADA COMPILACION DEL PROYECTO CON UN HASH
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
 
