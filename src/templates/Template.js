d065e4dd3df8666bd5faf4b80a54b8061b4b1ad5
sergio9815 <gsergio1598@gmail.com>
2021-04-14 21:11:15 -0500

    :bug: Fix bug

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -10,8 +10,8 @@ const Template = async () => {
       <div class="card">
         <div class="card_details">
           <div class="card_photo center circle">
-            <img src='${data.picture.large}'/>
-            <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
+            <img src="${data.picture.large}" alt="${data.name.first}"/>
+            <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194"
               xml:space="preserve">
               <circle cx="50" cy="50" r="40" />
             </svg>
